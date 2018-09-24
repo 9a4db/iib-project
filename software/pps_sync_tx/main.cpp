fa6152c1a60468f898c549eb9cb8ebc2de86e73a
Matt Coates <mc955@cam.ac.uk>
2018-09-24 02:30:58 +0100

    tx/rx kind of works

--- software/pps_sync_tx/main.cpp
+++ software/pps_sync_tx/main.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-// g++ main.cpp tranciever_setup.cpp -std=c++11 -lLimeSuite -o tx-pps.out
+// g++ main.cpp tranciever_setup.cpp -std=c++11 -lLimeSuite -o pps-tx-rx.out
 
 /* Entry Point */
 int main(int argc, char** argv){
@@ -38,13 +38,32 @@ int main(int argc, char** argv){
 
     configure_tranciever(config);
 
+    
+    /* RX Stream Config  */
+    lms_stream_t rx_stream;
+    rx_stream.channel = 0;                           // Channel Number
+    rx_stream.fifoSize = 1360 * 2048;                // Fifo Size in Samples
+    rx_stream.throughputVsLatency = 1.0;             // Optimize Throughput (1.0) or Latency (0)
+    rx_stream.isTx = false;                          // TX/RX Channel
+    rx_stream.dataFmt = lms_stream_t::LMS_FMT_I12;   // Data Format - int16_t
+    LMS_SetupStream(device, &rx_stream);
+
+    /* RX Data Buffer */
+    const int num_rx_samples = 1360;
+    const int rx_buffer_size = num_rx_samples * 2;
+    int16_t rx_buffer[rx_buffer_size];
+
+    /* RX Stream Metadata */
+    lms_stream_meta_t rx_metadata;
+
+    
     /* TX Stream Config */
     lms_stream_t tx_stream;
     tx_stream.channel = 0;                           // Channel Number
-    tx_stream.fifoSize = 1024 * 256;                 // Fifo Size in Samples
+    tx_stream.fifoSize = 1024 * 1024;                 // Fifo Size in Samples
     tx_stream.throughputVsLatency = 1.0;             // Optimize Throughput (1.0) or Latency (0)
     tx_stream.isTx = true;                           // TX/RX Channel
-    tx_stream.dataFmt = lms_stream_t::LMS_FMT_I16;   // Data Format - int16_t
+    tx_stream.dataFmt = lms_stream_t::LMS_FMT_I12;   // Data Format - int16_t
     LMS_SetupStream(device, &tx_stream);
 
     /* TX Data Buffer */
@@ -57,49 +76,71 @@ int main(int argc, char** argv){
     tx_metadata.flushPartialPacket = false;          // Dont force sending of incomplete packets
     tx_metadata.waitForTimestamp = false;            // Disable synchronization to HW timestamp
 
-
-
-    /* Generate Test Signal */
+    /* Generate TX Test Signal */
     const double tone_freq = 1e6;
     for (int i = 0; i <num_tx_samples; i++){
         const double pi = acos(-1);
         double w = 2*pi*i*tone_freq/config.sample_rate;
-        tx_buffer[2*i] = (int16_t)(cos(w)*32767.0f);
-        tx_buffer[2*i+1] = (int16_t)(sin(w)*32767.0f);
+        tx_buffer[2*i] = (int16_t)(cos(w)*2048.0f);
+        tx_buffer[2*i+1] = (int16_t)(sin(w)*2048.0f);
     }
 
-    
+
+    /* Output File */
+    ofstream data_file;
+    data_file.open("test.bin", std::ofstream::binary);
+
     /* Start Streams */
     LMS_StartStream(&tx_stream);
-    
-    
-    
-    /* Run for 10s */
+    LMS_StartStream(&rx_stream);
+
+    /* Process Stream */
     auto t1 = chrono::high_resolution_clock::now();
     auto t2 = t1;
     while (chrono::high_resolution_clock::now() - t1 < chrono::seconds(10)){
         
-        /* Transmit Samples */
-        if (LMS_SendStream(&tx_stream, tx_buffer, num_tx_samples, &tx_metadata, 1000) != num_tx_samples)
+        /* Recieve Samples */
+        if (LMS_RecvStream(&rx_stream, rx_buffer, num_rx_samples, &rx_metadata, 1000) != num_rx_samples){
+            cout << "RX Stream Error" << endl;
             error();
+        }
+
+        /* Transmit Samples 
+        if (LMS_SendStream(&tx_stream, tx_buffer, num_tx_samples, &tx_metadata, 1000) != num_tx_samples){
+            cout << "TX Stream Error" << endl;
+            error();
+        }*/
         
+        /* Write Samples to File */
+        if((rx_metadata.timestamp > 60440000) && (rx_metadata.timestamp < 60453600)){
+            data_file.write((char*)rx_buffer, sizeof(rx_buffer));
+            cout << "out\n"; 
+        }
+
         /* Print Stats Every Second */
         if (chrono::high_resolution_clock::now() - t2 > chrono::seconds(1)){
             t2 = chrono::high_resolution_clock::now();
             lms_stream_status_t status;
             LMS_GetStreamStatus(&tx_stream, &status);
-            cout << "\nTX data rate: " << status.linkRate / 1e6 << " MB/s\n";
+            cout << "\nTX Data rate: " << status.linkRate / 1e6 << " MB/s\n";
             cout << "Dropped TX packets: " << status.droppedPackets << endl;
+            LMS_GetStreamStatus(&rx_stream, &status);
+            cout << "RX Data rate: " << status.linkRate / 1e6 << " MB/s\n";
+            cout << "Dropped RX packets: " << status.droppedPackets << endl;
+            cout << "RX Timestamp: " << rx_metadata.timestamp << endl;
         }
     }
-
-
-    
+  
     /* Stop Streaming */
+    LMS_StopStream(&rx_stream);
     LMS_StopStream(&tx_stream);
     
     /* Destroy Stream */
-    LMS_DestroyStream(device, &tx_stream);    
+    LMS_DestroyStream(device, &rx_stream);
+    LMS_DestroyStream(device, &tx_stream);   
+
+    /* Close File */
+    data_file.close();
     
     /* Disable TX/RX Channels */
     if (LMS_EnableChannel(device, LMS_CH_TX, 0, false)!=0)
---
+++ software/pps_sync_tx/sample_plot.py
@@ -0,0 +1,41 @@
+import sys
+import struct
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+# Useage
+if len(sys.argv) != 2:
+    print("Usage: {} <samples.bin>".format(sys.argv[0]))
+    sys.exit(1)
+    
+# Open Datafile
+file = open(sys.argv[1], 'rb')
+    
+# Compute Number of Samples Present
+file.read()
+num_samples = int((file.tell())/4)
+file.seek(0)
+print("File contains", num_samples, "samples.")
+dur = (1/30.72)*num_samples
+print("Duration: %.4f us" % dur)
+print("")
+
+# Create I and Q Arrays
+I = np.zeros(num_samples, dtype=float)
+Q = np.zeros(num_samples, dtype=float)
+for n in range(0, num_samples):
+    
+    I[n] = struct.unpack('h', file.read(2))[0]
+    Q[n] = struct.unpack('h', file.read(2))[0]
+
+# Creat Complex Array
+samples = (I + 1j*Q)
+print(samples)
+
+# Plot I&Q Channels
+plt.plot(np.real(samples), label='I')
+plt.plot(np.imag(samples), label='Q')
+plt.grid(True)  
+plt.legend(loc='upper right', frameon=True)
+plt.show()
