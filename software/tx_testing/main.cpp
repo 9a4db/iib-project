4fb8fb9ad54e3ca6bb52b71e7b2594f6cb28ca27
Matt Coates <mc955@cam.ac.uk>
2018-09-26 23:50:10 +0100

    scheduled tx of single buffer appears to work

--- software/tx_testing/main.cpp
+++ software/tx_testing/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char** argv){
     config.rx_centre_frequency = 868e6;                 // RX Center Freuency    
     config.rx_antenna = LMS_PATH_LNAW;                  // RX RF Path = 10MHz - 2GHz
     config.rx_gain = 0.7;                               // RX Normalised Gain - 0 to 1.0
-    config.enable_rx_LPF = false;                       // Enable RX Low Pass Filter
+    config.enable_rx_LPF = true;                        // Enable RX Low Pass Filter
     config.rx_LPF_bandwidth = 10e6;                     // RX Analog Low Pass Filter Bandwidth
     config.enable_rx_cal = true;                        // Enable RX Calibration
     config.rx_cal_bandwidth = 8e6;                      // Automatic Calibration Bandwidth
@@ -28,12 +28,12 @@ int main(int argc, char** argv){
     config.tx_centre_frequency = 868e6;                 // TX Center Freuency
     config.tx_antenna = LMS_PATH_TX2;                   // TX RF Path = 10MHz - 2GHz
     config.tx_gain = 0.4;                               // TX Normalised Gain - 0 to 1.0
-    config.enable_tx_LPF = false;                        // Enable TX Low Pass Filter
-    config.tx_LPF_bandwidth = 10e6;                      // TX Analog Low Pass Filter Bandwidth
+    config.enable_tx_LPF = true;                        // Enable TX Low Pass Filter
+    config.tx_LPF_bandwidth = 10e6;                     // TX Analog Low Pass Filter Bandwidth
     config.enable_tx_cal = true;                        // Enable TX Calibration
     config.tx_cal_bandwidth = 8e6;                      // Automatic Calibration Bandwidth
     
-    config.sample_rate = 20e6;                          // Device Sample Rate 
+    config.sample_rate = 30.72e6;                       // Device Sample Rate 
     config.rf_oversample_ratio = 4;                     // ADC Oversample Ratio
     
     configure_tranciever(config);
